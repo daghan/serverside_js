88be5e40b7d8ffa892998ff3f3f30c4c943777f2
Daghan Altas <daghan@gmail.com>
2019-08-26 09:09:36 -0700

    HW 2

---
+++ conFusionServer/models/promotions.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+// {
+//   "name": "Weekend Grand Buffet",
+//   "image": "images/buffet.png",
+//   "label": "New",
+//   "price": "19.99",
+//   "description": "Featuring . . .",
+//   "featured": false
+// }
+
+const promoSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ""
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    featured: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+var Promotions = mongoose.model("Promo", promoSchema);
+module.exports = Promotions;
