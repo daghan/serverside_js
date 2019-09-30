fe542998866b8f4faeb28aab1d48e052b40c9a81
Daghan Altas <daghan@gmail.com>
2019-09-30 07:32:58 -0700

    Mongoose Population

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -22,10 +22,20 @@ router.post("/signup", (req, res, next) => {
         res.setHeader("Content-Type", "application/json");
         res.json({ err: err });
       } else {
-        passport.authenticate("local")(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "Registration Successful!" });
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
         });
       }
     }
@@ -43,8 +53,9 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
   });
 });
 
-router.get("/logout", (req, res) => {
+router.get("/logout", authenticate.verifyUser, (req, res, next) => {
   if (req.session) {
+    console.log("I am here");
     req.session.destroy();
     res.clearCookie("session-id");
     res.redirect("/");
